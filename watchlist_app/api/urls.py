bc6dbaa9550dfe882eddcbbabf667da183e8a66a
Rabib Galib <rabib@genex.digital>
2021-11-23 00:45:05 +0600

    Serializer

---
+++ watchlist_app/api/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+# from watchlist_app.api.views import movie_list, movie_details
+from watchlist_app.api.views import WatchListAV, WatchDetailAV, StreamPlatformAV
+
+urlpatterns = [
+    path('list/', WatchListAV.as_view(), name='movie-list'),
+    path('<int:pk>', WatchDetailAV.as_view(), name='movie_details'),
+    path('stream/', StreamPlatformAV.as_view(), name='stream'),
+]
